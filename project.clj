8c20e049ef92039a93fca0e872d2cdac735a8f50
kirankarkera <kiran.karkera@bridgei2i.com>
2015-03-26 16:14:56 +0530

    changed project name, added artifact, group name

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojure-word2vec "0.1.0-SNAPSHOT"
-  :description "A Clojure wrapper for a word2vecJava implementation"
-  :url "http://example.com/FIXME"
+(defproject org.bridgei2i/word2vec "0.1.0"
+  :description "A Clojure wrapper for the Medallia word2vecJava implementation"
+  :url "https://github.com/Bridgei2i/clojure-word2vec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
